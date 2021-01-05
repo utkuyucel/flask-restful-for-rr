a7e505d69c9e1412cef24b5188c0440b504cff2f
UtkuY <utkuyucel35@gmail.com>
2021-01-05 15:06:02 +0300

    Update run.py
    ALPHA value has added

--- main/run.py
+++ main/run.py
@@ -5,25 +5,30 @@ from dev_value import Calculate
 app = Flask(__name__)
 api = Api(app)
 
+ALPHA = 3.5
 
 class Out(Resource):
 	def get(self):
 		return {"about":"This is a Rest API Server for calculating the optimal value !"}
 
 	def post(self):
-		jjon = request.get_json()
-		return {"sent:", jjon}, 201
+		some_json = request.get_json()
+		return {"you sent:", some_json}, 201
 
 
 class Multi(Resource):
-	def get(self, total, num):
-		calculate = Calculate(num, total, 4)._run()
-		percentage = round((100 * (calculate / total)), 2)
-		return jsonify({"value":calculate, "percentage":percentage})
+	def get(self, total ,num):
+		calculate = Calculate(num, total, ALPHA)._run()
+		percentage = round((100 * (calculate/total)), 2)
+		return jsonify({
+		    "value":calculate,
+		    "percentage":percentage,
+		    "alpha":ALPHA,
+		    })
 
 api.add_resource(Out, "/")
 api.add_resource(Multi, "/rr/<int:total>/<float:num>")
 
 
 if __name__ == "__main__":
-	app.run(debug = True)
+	app.run()
